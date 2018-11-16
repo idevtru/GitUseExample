f94ff871c592fed200bb17ba9a325077a586cd50
Rich Castagna <idev.tru@gmail.com>
2018-11-16 13:02:53 -0500

    Add new Map-For_Itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Richard Castagna on 11/16/18.
+//  Copyright © 2018 Richard Castagna. All rights reserved.
+//
+
+import Foundation
